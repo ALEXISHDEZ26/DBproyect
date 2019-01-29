0c6c63bc95df2ba2e1395f2bec9d2570deec7bc9
Alexis Hernandez <hernandez_alexis.95@outlook.es>
2019-01-28 19:34:20 -0600

    connexion con base de datos

--- DBproyect/server.js
+++ DBproyect/server.js
@@ -4,20 +4,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient
 var db
 
-MongoClient.connect('mongodb+srv://alexis:<rock260795>@mydatabase-rdzfu.mongodb.net/admin', (err, client) => {
-  if (err) return console.log(err)
-  db = client.db('firstDB.DB1') // whatever your database name is
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
-})
-
 app.use(bodyParser.urlencoded({extended: true}))
-
-
-app.listen(3000,function() {
-    console.log('listening on 3000')
-})
 
 app.get('/', (req, res) => {    // aplicacion get(read)
     res.sendFile(__dirname + '/index.html')}  //res es la respuesta que enviara el server cuando obtenga una coincidencia en este caso "/" y enviara el archivo html
@@ -31,8 +18,12 @@ app.post('/quotes', (req, res) => {
        console.log('saved to database')
        res.redirect('/')
     })
+})
 
-
-
-
+MongoClient.connect('mongodb+srv://alexis:rock260795@mydatabase-rdzfu.mongodb.net/test?retryWrites=true', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('firstDB') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
